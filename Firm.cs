5a8b9c278ce62ddb834edb03ed1936e322a9d3d5
kirilenkovmaksim-web <kirilenkovmaksim@gmail.com>
2026-05-04 15:03:05 +0300

    Добавлен класс Firm

--- Firm.cs
+++ Firm.cs
@@ -8,5 +8,23 @@ namespace Pr1
 {
     internal class Firm
     {
+        string Name { get; set; }
+        string Email { get; set; }
+        int GotCreate { get; set; }
+
+        public Firm(string name, string email, int age)
+        {
+            Name = name;
+            Email = email;
+            GotCreate = age;
+        }
+
+        public void PrintInfo()
+        {
+            Console.WriteLine($"Название фирмы: {Name}");
+            Console.WriteLine($"Адрес: {Email}");
+            Console.WriteLine($"Год основания: {GotCreate}");
+        }
+
     }
 }
