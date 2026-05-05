fe4415fc7996e539199be211cd891fa7882c00e5
kirilenkovmaksim-web <kirilenkovmaksim@gmail.com>
2026-05-05 11:16:29 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -5,8 +5,10 @@ internal class Program
 {
     static void Main()
     {
-        Firm f = new Firm("Apple", "ул. Ленина, 10", 2000);
+         Firm f = new Firm("Apple", "ул. Ленина, 10", 2000);
         f.PrintInfo();
     }
 
 }
+
+
